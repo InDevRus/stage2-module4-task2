8a1a25a67c32276a6e83d2e5d26492f000019a4a
indev <InDevRus@hotmail.com>
2023-09-03 04:35:13 +0000

    Implemented required functionality

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -1,15 +1,30 @@
 package com.mjc.stage2.impl;
 
 
-public class ProxyConnection {
-    private RealConnection realConnection;
+import com.mjc.stage2.Connection;
+
+public class ProxyConnection implements Connection {
+    private final RealConnection realConnection;
+
+    private boolean isClosed = false;
 
     public ProxyConnection(RealConnection realConnection) {
         this.realConnection = realConnection;
     }
 
     public void reallyClose() {
-        // Write your code here!
+        isClosed = true;
+        ConnectionPool.getInstance().releaseConnection(realConnection);
     }
-    // Implement methods here!
+
+    @Override
+    public void close() {
+        isClosed = true;
+        ConnectionPool.getInstance().releaseConnection(this);
+    }
+
+    @Override
+    public boolean isClosed() {
+        return isClosed;
+    }
 }
